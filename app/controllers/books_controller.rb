86d11aae78b95e087784ce26ef96ded343b4a421
EC2 Default User <ec2-user@ip-172-31-31-59.ec2.internal>
2021-03-04 14:33:13 +0000

    [Add]headerの編集

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,20 @@
+class BooksController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'homes#top'
+  get 'home/about' => 'homes#about'
   devise_for :users
+  resources :users,only:[:index, :show, :edit, :update]
+  resources :books,only:[:index, :show, :edit, :create, :update, :destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
